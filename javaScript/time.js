0eeda12e55f0fbf866f97789469562098851ee0c
EUNHEE JANG <6810779s@naver.com>
2021-08-30 18:31:47 +0900

    add clock

--- javaScript/time.js
+++ javaScript/time.js
@@ -1,10 +1,20 @@
-const getTime = document.querySelector(".js-clock"),
-  clockTitle = getTime.querySelector(h1);
+const time = document.querySelector(".js-clock"),
+  clockTitle = time.querySelector("h1");
 
-setInterval(() => {
-  
-}, 1000);
+console.log(time);
+// setInterval(() => {}, 1000);
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours <= 9 ? `0${hours}` : hours}:${
+    minutes <= 9 ? `0${minutes}` : minutes
+  }:${seconds <= 9 ? `0${seconds}` : seconds}`;
 }
+
+function init() {
+  setInterval(getTime, 1000);
+}
+
+init();
