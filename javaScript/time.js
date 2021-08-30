ada7e9df32cfe31fb88fc6bcb095f9aa425c966e
EUNHEE JANG <6810779s@naver.com>
2021-08-30 18:17:27 +0900

    first update code

---
+++ javaScript/time.js
@@ -0,0 +1,10 @@
+const getTime = document.querySelector(".js-clock"),
+  clockTitle = getTime.querySelector(h1);
+
+setInterval(() => {
+  
+}, 1000);
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+}
