d99bddd45fbe6c0fd62836611be6fd0900d6a1ac
EUNHEE JANG <6810779s@naver.com>
2021-08-31 10:08:49 +0900

    style

---
+++ javaScript/bg.js
@@ -0,0 +1,26 @@
+const body = document.querySelector("body");
+const IMG_NUMBER = 2;
+
+// function handleImgLoad() {
+//   console.log("finish loading");
+// }
+
+function paintImage(imgNumber) {
+  const img = new Image();
+  img.src = `./img/bg${imgNumber + 1}.jpg`;
+  img.classList.add("bgImg");
+  body.appendChild(img);
+
+  // img.addEventListener("loadend", handleImgLoad);
+}
+
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+init();
